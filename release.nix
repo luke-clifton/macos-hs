1cec894b4a101a88edbdf397d920cf6aa261fa91
Luke Clifton <Luke.Clifton@cba.com.au>
2018-07-20 16:31:43 +0800

    darwin channel doesn't have 842

--- release.nix
+++ release.nix
@@ -1,6 +1,6 @@
 {nixpkgs ? import <nixpkgs> {}}:
 let
-  compilers = ["default" "ghc822" "ghc842"];
+  compilers = ["default" "ghc822" "ghc843"];
 in
   with nixpkgs.lib;
   genAttrs compilers (compiler: import ./. {inherit nixpkgs compiler;})
